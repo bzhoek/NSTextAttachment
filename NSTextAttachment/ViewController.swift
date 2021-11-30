c1112e08b7f463f5aecfa0b188c0a969d48e8447
Bas van der Hoek <bas@hoek.com>
2021-11-30 16:21:38 +0100

    Initial Commit

---
+++ NSTextAttachment/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  NSTextAttachment
+//
+//  Created by Bas van der Hoek on 30/11/2021.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
