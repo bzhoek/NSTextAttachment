fc38220d55a5dd06981651e2d466a3cd8753a2ca
Bas van der Hoek <bas@hoek.com>
2023-12-08 15:34:31 +0100

    Test text attachment cell

--- NSTextAttachment/ViewController.swift
+++ NSTextAttachment/ViewController.swift
@@ -7,12 +7,27 @@
 
 import Cocoa
 
+class ImageAttachmentCell: NSTextAttachmentCell {
+//  override func cellFrame(for textContainer: NSTextContainer, proposedLineFragment lineFrag: NSRect, glyphPosition position: NSPoint, characterIndex charIndex: Int) -> NSRect {
+//    NSRect(x: 0, y: 0, width: 16, height: 16)
+//  }
+  
+}
+
 class ViewController: NSViewController {
 
+  @IBOutlet var textView: NSTextView!
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
-    // Do any additional setup after loading the view.
+    
+    let attachment = NSTextAttachment()
+    textView.textStorage?.replaceCharacters(in: NSMakeRange(0, 0), with: "Hello, world")
+    let image = NSImage(contentsOfFile: "/Users/bas/zilverline/markdone-swift/unittest/bzhoek16.png")
+    print("\(image?.size)")
+    attachment.attachmentCell = ImageAttachmentCell(imageCell: image)
+    let attachmentString = NSAttributedString(attachment: attachment)
+    textView.textStorage?.insert(attachmentString, at: 1)
   }
 
   override var representedObject: Any? {
